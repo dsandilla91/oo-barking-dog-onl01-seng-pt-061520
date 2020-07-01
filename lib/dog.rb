8e1e4f7e29b1735261b2ec95ae1bba21fe682a47
Deborah Santillan <s.debb@yahoo.com>
2020-07-01 03:18:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 # Your code goes here!
 class Dog 
   def name=(dog_name)
-    pup_name = dog_name
+    @pup_name = dog_name
 end 
 
 def name
-  pup_name
+  @pup_name
 end 
 
+class Dog
 def bark 
    puts "Woof!" 
  end
